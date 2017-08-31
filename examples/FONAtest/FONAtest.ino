a632eaecf5e079c4aa6e056236433d275aa6daa1
Timothy Woo <botletics@gmail.com>
2017-08-31 00:07:46 -0400

    Update FONAtest.ino

--- examples/FONAtest/FONAtest.ino
+++ examples/FONAtest/FONAtest.ino
@@ -805,9 +805,6 @@ void loop() {
         break;
        }
     case '2': {
-        // Press "G" to enable GPRS before using this function!
-        // Also make sure to properly set up the bearer credentials (APN settings)
-      
         // Post data to website via 2G
         float temperature = analogRead(A0)*1.23; // Change this to suit your needs
         uint16_t battLevel = 87; // Just for testing. Use the read battery function instead
@@ -844,9 +841,9 @@ void loop() {
         break;
       }
     case '3': {
-        // Press "G" to enable GPRS before using this function!
-        // Also make sure to properly set up the bearer credentials (APN settings)
-      
+        // For this to work, make sure you uncomment line 57 to select FONA 3G!
+        // Also uncomment the line "if (!fona.postData3G(URL))" below
+        
         // Post data to website via 3G
         float temperature = analogRead(A0)*1.23; // Change this to suit your needs
         uint16_t battLevel = 87; // Just for testing. Use the read battery function instead
@@ -864,9 +861,9 @@ void loop() {
         // Use IMEI as device ID in this example:
         sprintf(URL, "GET /dweet/for/%s?temp=%s&batt=%s HTTP/1.1\r\nHost: dweet.io\r\nContent-Length: 0\r\n\r\n", imei, tempBuff, battLevelBuff);
 
-        if (!fona.postData3G(URL))
+//        if (!fona.postData3G(URL)) // Uncomment this if you have FONA 3G!
           Serial.println(F("Failed to post data to website..."));
-          
+        
         break;
       }
     /*****************************************/
